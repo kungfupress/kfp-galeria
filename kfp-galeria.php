5aa176b859d9be2de03c238049aa9b5b601a06cc
Juanan Ruiz <juanan@us.es>
2020-01-26 20:54:12 +0100

    Cambio a versión 1.2.0

--- kfp-galeria.php
+++ kfp-galeria.php
@@ -3,7 +3,7 @@
  * Plugin Name:   KFP Galeria
  * Description:   Crear un campo personalizado para mostrar una galeria
  * Plugin URI:    https://github.com/kungfupress/kfp-galeria
- * Version:       1.1.0
+ * Version:       1.2.0
  * Text Domain:   kfp-galeria
  * Plugin Author: Juanan Ruiz
  * Author URI:    https://kungfupress.com/
@@ -16,7 +16,7 @@ defined( 'ABSPATH' ) || die();
 define( 'KFP_GALERIA_DIR', plugin_dir_path( __FILE__ ) );
 define( 'KFP_GALERIA_PLUGIN_FILE', __FILE__ );
 define( 'KFP_GALERIA_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'KFP_GALERIA_VERSION', '1.1.0' );
+define( 'KFP_GALERIA_VERSION', '1.2.0' );
 
 require_once KFP_GALERIA_DIR . 'include/create-cpt-viaje.php';
 require_once KFP_GALERIA_DIR . 'include/create-metabox.php';
